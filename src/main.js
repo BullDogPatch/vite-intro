0a4b7d10002ae542691bb48ea953e462a9cd5176
Craig Clayton <craig__clayton@outlook.com>
2025-01-06 11:28:01 +0000

    change Vanilla logo to liverpool logo

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import liverpoolLogo from './liverpool-fc-4.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    <a href="https://www.liverpoolfc.com/" target="_blank">
+      <img src="${liverpoolLogo}" class="logo liverpool" alt="Liverpool FC logo" />
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click on the Liverpool FC logo to go to the Liverpool FC website.
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'));
